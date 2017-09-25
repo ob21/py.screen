a86aa2be6f7de1205a5699e804a4ad9c5650b7aa
OPOB7414 <olivier.briand@orange.com>
2017-09-25 16:55:46 +0200

    set 3 frames with 3 textes and refresh

--- screen.py
+++ screen.py
@@ -29,7 +29,40 @@ root.title("my app")
 
 root.config(bg="brown4")
 
-root.w = Label(root, text='hello world ! ' + strftime('%H:%M:%S'), fg="black", font=("Helvetica", 40), bg="red")
-root.w.pack()
+root.update()
+
+width = root.winfo_screenwidth()
+height = root.winfo_screenheight()
+
+print(str(width) + "x" + str(height))
+
+half_width = width/2
+half_height = height/2
+
+print(str(half_width) + "x" + str(half_height))
+
+frame1 = Frame(root, width=half_width, height=half_height, bg="red")
+frame1.grid(row=0, column=0)
+
+hour = Label(frame1, text=strftime('%H h %M'), fg="white", font=("Arial bold", 200), bg="black")
+hour.grid(row=0, column=0)
+
+frame2 = Frame(root, width=half_width, height=half_height, bg="blue")
+frame2.grid(row=0, column=1)
+
+# meteo = Label(frame2, text="meteo", fg="white", font=("Arial bold", 200), bg="black")
+# meteo.grid(row=0, column=1)
+
+frame3 = Frame(root, width=half_width, height=half_height, bg="green")
+frame3.grid(row=1, column=0)
+
+jour = Label(frame3, text="jour", fg="white", font=("Arial bold", 50), bg="black")
+jour.grid(row=1, column=0)
+
+def update():
+    hour.config(text=strftime('%H:%M'), fg="white", font=("Arial bold", 300), bg="black")
+    root.after(1000, update)
+
+update()
 
 root.mainloop()
